f89a9bfdb331c8e7037afdbbc34aa262e48974cc
Erin Sheldon <erin.sheldon@gmail.com>
2019-09-13 11:49:28 -0400

    tester for sim

---
+++ shredder/tests/test_sim.py
@@ -0,0 +1,19 @@
+import numpy as np
+import shredder
+import pytest
+
+
+@pytest.mark.parametrize('seed', [8, 314159])
+def test_sim_smoke(seed):
+    rng = np.random.RandomState(seed)
+    sim = shredder.sim.Sim(rng)
+    mbobs = sim()  # noqa
+
+
+@pytest.mark.parametrize('seed', [75, 817, 213])
+def test_sim(seed):
+    rng = np.random.RandomState(seed)
+    sim = shredder.sim.Sim(rng)
+    mbobs = sim()
+
+    assert mbobs.meta['centers'].size == sim['objects']['nobj']
