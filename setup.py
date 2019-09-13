29aba004e3879d3a87ab986307bd45583c2ed11b
Erin Sheldon <erin.sheldon@gmail.com>
2019-09-13 06:23:38 -0400

    some initial commits in shredder

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='shredder',
+    version='v0.1.0',
+    description='Astronomical deblender',
+    license='GPL',
+    author='Erin Scott Sheldon',
+    author_email='erin.sheldon@gmail.com',
+    packages=['shredder'],
+)
