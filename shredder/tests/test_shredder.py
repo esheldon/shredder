18497029edd33a374add4633317e6abc9018fb61
Erin Sheldon <erin.sheldon@gmail.com>
2019-09-13 11:55:34 -0400

    add test of the performance agains the sim
    make sure chi2/dof < 1.05

--- shredder/tests/test_shredder.py
+++ shredder/tests/test_shredder.py
@@ -38,3 +38,53 @@ def test_shredder_smoke(seed, show=False):
 
     if show:
         s.view_comparison(scale=2, show=True)
+
+
+@pytest.mark.parametrize('seed', [125, 871])
+def test_shredder(seed):
+    """
+    test that the fit is pretty good
+    """
+    rng = np.random.RandomState(seed)
+    sim = shredder.sim.Sim(rng)
+    mbobs = sim()
+
+    scale = sim['image']['pixel_scale']
+
+    centers = mbobs.meta['centers']
+    add_dt = [('T', 'f8')]
+    objs = eu.numpy_util.add_fields(centers, add_dt)
+
+    # fake size guesses
+    objs['T'] = rng.uniform(
+        low=2.0*scale**2,
+        high=4.0*scale**2,
+        size=objs.size,
+    )
+
+    gm_guess = shredder.get_guess_from_cat(
+        objs,
+        pixel_scale=scale,
+        model='dev',
+        rng=rng,
+    )
+
+    s = shredder.Shredder(mbobs, rng=rng)
+    s.shred(gm_guess)
+
+    models = s.get_model_images()
+
+    chi2 = 0.0
+    dof = 0
+    for band, model in enumerate(models):
+        image = mbobs[band][0].image
+        dof += image.size
+
+        weight = mbobs[band][0].weight
+        diffim = image - model
+        chi2 += (diffim**2 * weight).sum()
+
+    dof = dof - 3
+    chi2per = chi2/dof
+
+    assert chi2per < 1.05
