51b843c6ab7a21c2a77eeb1fa5b89b6688bf7173
Erin Sheldon <erin.sheldon@gmail.com>
2019-09-19 16:49:52 -0400

    only way to change psf

--- shredder/tests/test_shredder.py
+++ shredder/tests/test_shredder.py
@@ -39,9 +39,8 @@ def test_shredder_smoke(seed, vary_sky, show=False):
     test we can run end to end
     """
     rng = np.random.RandomState(seed)
-    sim = shredder.sim.Sim(rng=rng)
-    sim['psf']['model'] = 'gauss'
-    sim['psf']['fwhm'] = 0.9
+    conf={'psf': {'model':'gauss', 'fwhm':0.9}}
+    sim = shredder.sim.Sim(rng=rng, config=conf)
     sim['objects']['flux_range'] = [100, 200]
     sim['objects']['hlr_range'] = (0.0001, 0.0001)
     mbobs = sim()
