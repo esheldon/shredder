4cbe9b24fa7c24c6ebe0264635b8fdc85e358c06
Erin Sheldon <erin.sheldon@gmail.com>
2022-10-18 10:47:04 -0400

    add license and update version to 0.9.1

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from distutils.core import setup
 
 setup(
     name='shredder',
-    version='v0.9.0',
-    description='Astronomical image deblender',
+    version='v0.9.1',
+    url="https://github.com/esheldon/shredder",
+    description='An astronomical image deblender',
     license='GPL',
     author='Erin Scott Sheldon',
     author_email='erin.sheldon@gmail.com',
--- shredder/__init__.py
+++ shredder/__init__.py
@@ -1,4 +1,4 @@
-__version__ = 'v0.9.0'
+__version__ = 'v0.9.1'
 
 from . import em
 from . import coadding
